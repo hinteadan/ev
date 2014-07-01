46eb5f0b63914c473f9fbc7fd127ce919efafdbc
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-07-01 16:55:39 +0300

    Scaffold UI Router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 (function (angular) {
     'use strict';
 
-    angular.module('eye-view', ['ngRoute']);
+    angular.module('eye-view', ['ngRoute'])
+    .service('uiRouter', ['$window', function ($w) {
+        this.route = function () {
+            $w.location.href = 'index_patient.html';
+        };
+    }])
+    .controller('uiRouter', ['uiRouter', function (router) {
+        router.route();
+    }]);
 
 }).call(this, this.angular);
