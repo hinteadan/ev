8f2dea4498c5e37be05c47ed961b554856a79619
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-07-01 17:07:02 +0300

    Scaffold Patient module

--- app/scripts/patient/module.js
+++ app/scripts/patient/module.js
@@ -1,6 +1,13 @@
 ﻿(function (angular) {
     'use strict';
 
-    angular.module('eye-view-patient', ['ngRoute']);
+    angular.module('eye-view-patient', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+
+        $routeProvider
+            .when('/', { templateUrl: 'scripts/patient/talk-thread.tmpl.html', controller: 'talkThreadCtrl' })
+            .otherwise({ redirectTo: '/' });
+
+    }]);
 
 }).call(this, this.angular);
---
+++ app/scripts/patient/talkThreadCtrl.js
@@ -0,0 +1,9 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('eye-view-patient')
+    .controller('talkThreadCtrl', [function () {
+
+    }]);
+
+}).call(this, this.angular);
