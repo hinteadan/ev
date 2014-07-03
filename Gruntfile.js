ba61c6b113d7a8bd0cce3617ee4c8688befa478b
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-07-03 23:44:09 +0300

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,7 @@ module.exports = function (grunt) {
 		// concat, minify and revision files. Creates configurations in memory so
 		// additional tasks can operate on them
 		useminPrepare: {
-			html: ['<%= yeoman.app %>/index.html', '<%= yeoman.app %>/tabletennis.html'],
+			html: ['<%= yeoman.app %>/index*.html'],
 			options: {
 				dest: '<%= yeoman.dist %>',
 				flow: {
