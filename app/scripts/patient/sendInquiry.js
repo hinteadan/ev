dcb8ae7000261a895d7941cbe5b29cb0f0d1d473
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-07-03 11:08:36 +0300

    Scaffold Send inquiry

---
+++ app/scripts/patient/sendInquiry.js
@@ -0,0 +1,9 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('eye-view-patient')
+    .controller('sendInquiryCtrl', [function () {
+
+    }]);
+
+}).call(this, this.angular);
--- app/scripts/patient/talkThreadCtrl.js
+++ app/scripts/patient/talkThreadCtrl.js
@@ -2,8 +2,10 @@
     'use strict';
 
     angular.module('eye-view-patient')
-    .controller('talkThreadCtrl', [function () {
-
+    .controller('talkThreadCtrl', ['$scope', '$location', function ($s, $loc) {
+        $s.inquire = function () {
+            $loc.path('/inquire');
+        };
     }]);
 
 }).call(this, this.angular);
