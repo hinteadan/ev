47f9a304bba0bf4674be5ffd8602e65a45c47088
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-07-05 23:04:21 +0300

    Integrate notifier in the message flow

--- app/scripts/common/messenger.js
+++ app/scripts/common/messenger.js
@@ -1,53 +1,54 @@
 ﻿(function (angular, hds, _) {
-    'use strict';
+	'use strict';
 
-    var is = hds.is;
+	var is = hds.is;
 
-    angular.module('eye-view-common')
-    .service('messenger', ['$upload', '$q', 'dataStore', function ($upload, $q, ds) {
-        
-        this.uploadImage = function (file, data) {
-            return $upload.upload({
-                url: ds.blob.UploadUrl(),
-                data: data,
-                file: file
-            });
-        };
-        this.imageUrl = function (id) {
-            return ds.blob.UrlFor(id);
-        };
+	angular.module('eye-view-common')
+    .service('messenger', ['$upload', '$q', 'dataStore', 'notifier', 'NotifiyUser', function ($upload, $q, ds, notify, NotifiyUser) {
 
-        this.send = function (message) {
-        	var deferred = $q.defer();
+    	this.uploadImage = function (file, data) {
+    		return $upload.upload({
+    			url: ds.blob.UploadUrl(),
+    			data: data,
+    			file: file
+    		});
+    	};
+    	this.imageUrl = function (id) {
+    		return ds.blob.UrlFor(id);
+    	};
 
-        	ds.store.Save(new hds.Entity(message, message.meta()), function (result) {
-        		/// <param name='result' type='hds.OperationResult' />
-        		if (!result.isSuccess) {
-        			deferred.reject(result.reason);
-        			return;
-        		}
-        		deferred.resolve(result.data);
-        	});
+    	this.send = function (message) {
+    		var deferred = $q.defer();
 
-        	return deferred.promise;
-        };
+    		ds.store.Save(new hds.Entity(message, message.meta()), function (result) {
+    			/// <param name='result' type='hds.OperationResult' />
+    			if (!result.isSuccess) {
+    				deferred.reject(result.reason);
+    				return;
+    			}
+    			notify.user(new NotifiyUser(message.patientId, message.patientId, 'hintea_dan@yahoo.co.uk'));
+    			deferred.resolve(result.data);
+    		});
 
-        this.threadForUser = function (userId) {
-        	var deferred = $q.defer(),
+    		return deferred.promise;
+    	};
+
+    	this.threadForUser = function (userId) {
+    		var deferred = $q.defer(),
         		query = hds.queryWithAnd()
 				.where('patientId')(is.EqualTo)(userId);
 
-        	ds.store.Query(query, function(result){
-        		/// <param name='result' type='hds.OperationResult' />
-        		if (!result.isSuccess) {
-        			deferred.reject(result.reason);
-        			return;
-        		}
-        		deferred.resolve(_(result.data).reverse().value());
-        	});
+    		ds.store.Query(query, function (result) {
+    			/// <param name='result' type='hds.OperationResult' />
+    			if (!result.isSuccess) {
+    				deferred.reject(result.reason);
+    				return;
+    			}
+    			deferred.resolve(_(result.data).reverse().value());
+    		});
 
-        	return deferred.promise;
-        };
+    		return deferred.promise;
+    	};
 
     }]);
 
---
+++ app/scripts/common/notifier.js
@@ -0,0 +1,25 @@
+﻿(function (angular) {
+	'use strict';
+
+	var c = this.console;
+
+	function User(username, name, email) {
+		this.email = email || null;
+		this.name = name  || null;
+		this.username = username || null;
+	}
+
+	function ConsoleNotifier() {
+
+		this.user = function (user) {
+			/// <param name='user' type='User' />
+			c.info('To ' + user.name + ': You have a new reply from Eye-View.');
+		};
+
+	}
+
+	angular.module('eye-view-common')
+	.value('NotifiyUser', User)
+	.service('notifier', [ConsoleNotifier]);
+
+}).call(this, this.angular);
