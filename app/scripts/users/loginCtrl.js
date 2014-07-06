89a1dc2f5b127e2371fd5b1f6b240141aefb0901
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-07-06 13:12:20 +0300

    Login

--- app/scripts/users/loginCtrl.js
+++ app/scripts/users/loginCtrl.js
@@ -1,9 +1,23 @@
 ﻿(function (angular) {
 	'use strict';
 
-	angular.module('eye-view-users')
-	.controller('login', [function () {
+	var alert = this.alert,
+		log = this.console.log;
 
+	angular.module('eye-view-users')
+	.controller('login', ['$scope', '$w', 'authenticator', function ($s, $w, auth) {
+		$s.username = null;
+		$s.password = null;
+		$s.submit = function (form) {
+			if (!form.$valid) {
+				return;
+			}
+			auth.login($s.username, $s.password).then(function (currentUser) {
+				$w.location.href = '';
+			}, function (reason) {
+				alert(reason);
+			});
+		};
 	}]);
 
 }).call(this, this.angular);
