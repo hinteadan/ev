aa116526ae7f49bea4187a2d8d0553aa18bf4276
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-08-18 04:07:28 +0300

    Switch languages on index page

--- app/scripts/common/languageCtrl.js
+++ app/scripts/common/languageCtrl.js
@@ -3,8 +3,17 @@
 
 	angular.module('eye-view-common')
     .controller('languageCtrl', ['$scope', '$translate', function ($s, $t) {
+
+    	var currentLanguage = $t.preferredLanguage();
+
     	$s.setLanguage = function (lang) {
     		$t.use(lang);
+    		currentLanguage = lang;
+    	};
+
+    	$s.currentLanguage = function () {
+    		console.log(currentLanguage);
+    		return currentLanguage;
     	};
     }]);
 
