da6a4cee8b3c62de9f52a98e52f0eb0a3098ff40
Hintea Dan Alexandru <hinteadan@gmail.com>
2014-08-18 03:52:31 +0300

    Define Language controller

---
+++ app/scripts/common/languageCtrl.js
@@ -0,0 +1,11 @@
+﻿(function (angular) {
+	'use strict';
+
+	angular.module('eye-view-common')
+    .controller('languageCtrl', ['$scope', '$translate', function ($s, $t) {
+    	$s.setLanguage = function (lang) {
+    		$t.use(lang);
+    	};
+    }]);
+
+}).call(this, this.angular);
