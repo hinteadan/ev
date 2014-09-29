c02aa5f726595bf9150848a407931e2a50403697
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2014-09-29 13:31:18 +0300

    Add Terms and Conditions on logged-on pages

---
+++ app/scripts/common/termsCtrl.js
@@ -0,0 +1,11 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('eye-view-common')
+    .controller('termsCtrl', ['$scope', '$window', function ($s, $w) {
+        $s.viewTerms = function () {
+            $w.open('/tc.html', '_blank');
+        };
+    }]);
+
+}).call(this, this.angular);
